ee1215a85f4feda4beb53016e723b0580cd6ba77
Michel <mfacen@hotmail.com>
2018-07-11 19:08:52 -0400

    O

--- controls.h
+++ controls.h
@@ -2,9 +2,10 @@
 
 class Controls {
   public:
-  Input input;
-  Output output;
+  String name;
+  Input* input;
+  Output* output;
   String op = ">";
-  Controls(String n,Input in, Output out){input=in;output=out;name=n;}{};
+  Controls(String n,Input* in, Output* out):input{in},output{out},name{n}{};
 };
 
