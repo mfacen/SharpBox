fd23092e0583ff165fff222adcdac023cc462256
Michel <mfacen@hotmail.com>
2018-12-08 02:22:59 -0600

    Agregué que se puedan poner strings en el Page

---
+++ data/javascript.js
@@ -0,0 +1,25 @@
+ function btnClick(btn){
+
+    elementName=btn.id;elementValue=btn.value;
+
+    
+    if ( elementName == "switchToStation") {
+        var credentials = prompt("Por favor ingrese el SSID y contraseña separados por una coma.", "Primus-pfg,19771018");
+        elementValue = credentials;
+    }
+
+
+      var xhttp = new XMLHttpRequest(); 
+   xhttp.onreadystatechange = function()
+   { if (this.readyState == 4 && this.status == 200) {console.log(this.responseText);eval(this.responseText)}
+}; xhttp.open('GET', 'btnClick?button='+ elementName + '&value=' + elementValue , true);xhttp.send();};
+function btnClickText(elementName,elementValue){var xhttp = new XMLHttpRequest();
+   xhttp.onreadystatechange = function() { if (this.readyState == 4 && this.status == 200) {eval(this.responseText);}
+}; xhttp.open('GET', 'btnClick?button='+ elementName + '&value='+ elementValue +'', true);xhttp.send();}
+
+
+var connection = new WebSocket('ws://'+location.host+':81');
+connection.onopen = function(){ console.log('Connection open!');}
+connection.onclose = function(){console.log('Connection closed');}
+connection.onerror = function(error){   console.log('Error detected: ' + error);}
+connection.onmessage = function(e){   var server_message = e.data;  eval(server_message); }
